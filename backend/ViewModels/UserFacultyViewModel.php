78eb6bd98516954d14d0ad9c208154e4a786a504
Naskiiiieeeee <humblebeast1218@gmail.com>
2025-06-30 09:20:47 +0800

    UserFacultyViewModel setup

---
+++ backend/ViewModels/UserFacultyViewModel.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__ . '/../Models/UserFacultyModel.php';
+
+class UserFacultyViewModel {
+    private $model;
+
+    public function __construct() {
+        $this->model = new UserFaculty();
+    }
+
+    public function getPaginatedFaculty($page = 1, $limit = 4) {
+        $offset = ($page - 1) * $limit;
+        return $this->model->getFacultiesPaginated($offset, $limit);
+    }
+
+    public function getTotalPages($limit = 4) {
+        $totalRecords = $this->model->countAllFaculty();
+        return ceil($totalRecords / $limit);
+    }
+
+    public function deleteFacultyUser($id){
+        return $this->model->deleteFaculty($id);
+    }
+
+    public function updateAdminUser($id, $status){
+        return $this->model->updateStatus($id, $status);
+    }
+
+    public function addNewFaculty($path, $fullname, $email, $password, $dep, $role, $code){
+        return $this->model->createNewFaculty($path, $fullname, $email, $password, $dep, $role, $code);
+    }
+}
