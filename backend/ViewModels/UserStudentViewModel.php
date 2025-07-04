626b10147133fee2bb6f4cce167eb9e7fceef635
Naskiiiieeeee <humblebeast1218@gmail.com>
2025-07-04 12:30:27 +0800

    Update student view model

--- backend/ViewModels/UserStudentViewModel.php
+++ backend/ViewModels/UserStudentViewModel.php
@@ -10,4 +10,25 @@ class UserStudentViewModel {
     public function getUserEmail($email){
         return $this->model->getUserByEmail($email);
     }
+
+    public function deleteStudent($id){
+        return $this->model->deleteStudent($id);
+    }
+
+    public function addStudent($email, $studentNo, $fullname, $section, $yearLvl, $role){
+        return $this->model->createNewStudent($email, $studentNo, $fullname, $section, $yearLvl, $role);
+    }
+    public function updateStudent($id, $status){
+        return $this->model->updateStatus($id, $status);
+    }
+
+    public function getPaginatedFaculty($page = 1, $limit = 4) {
+        $offset = ($page - 1) * $limit;
+        return $this->model->getStudentPaginated($offset, $limit);
+    }
+
+    public function getTotalPages($limit = 4) {
+        $totalRecords = $this->model->countAllStudent();
+        return ceil($totalRecords / $limit);
+    }
 }
