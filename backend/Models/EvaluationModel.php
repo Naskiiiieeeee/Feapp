1026dab2016a645cdfd26772dd64da77bca2d2cb
Naskiiiieeeee <humblebeast1218@gmail.com>
2025-07-06 00:39:48 +0800

    Evaluation model, and viewModel setup successfully

---
+++ backend/Models/EvaluationModel.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__ . '/../Core/BaseModel.php';
+
+class EvaluationModel extends BaseModel{
+
+    public function countAllEvaluationsFromUsersInput($email){
+        $query = "SELECT COUNT(*) as total_records FROM `faculty_evaluations` WHERE `student_email` = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute([$email]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $row['total_records'];
+    }
+
+    public function getPaginatedEvaluationFromUsersInput($offset, $limit, $email) {
+        $query = "SELECT * FROM `faculty_evaluations` 
+                WHERE `student_email` = :email 
+                ORDER BY `id` DESC 
+                LIMIT :offset, :limit";
+
+        $stmt = $this->db->prepare($query);
+
+        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+        $stmt->bindValue(':offset', (int)$offset, PDO::PARAM_INT);
+        $stmt->bindValue(':limit', (int)$limit, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
---
+++ backend/ViewModels/EvaluationViewModel.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__ . '/../Models/EvaluationModel.php';
+
+class EvaluationViewModel {
+    private $model;
+
+    public function __construct(){
+        $this->model = new EvaluationModel();
+    }
+    public function getByStudentTofacultyEvaluation($email){
+        return $this->model->countAllEvaluationsFromUsersInput($email);
+    }
+
+    public function getPaginatedAdmins($page = 1, $limit = 4, $email) {
+        $offset = ($page - 1) * $limit;
+        return $this->model->getPaginatedEvaluationFromUsersInput($offset, $limit, $email);
+    }
+
+    
+}
