d204b36246f48111b325f13d11a6adc0b734856d
Naskiiiieeeee <humblebeast1218@gmail.com>
2025-07-20 23:13:46 +0800

    Sort faculty by department

--- frontend/views/student_users/FacultyViewList.php
+++ frontend/views/student_users/FacultyViewList.php
@@ -7,6 +7,16 @@ include_once __DIR__ . '/../../components/sidebar.php';
 $vm = new UserFacultyViewModel();
 $facultyList = null;
 $facultyList = $vm->getFacultyInfo();
+
+$groupedFaculty = [];
+foreach ($facultyList as $faculty) {
+    $dept = $faculty['department'];
+    if (!isset($groupedFaculty[$dept])) {
+        $groupedFaculty[$dept] = [];
+    }
+    $groupedFaculty[$dept][] = $faculty;
+}
+
 ?>
 
 <main id="main" class="main">
@@ -19,12 +29,17 @@ $facultyList = $vm->getFacultyInfo();
     </nav>
   </div>
 
-  <section class="section profile">
-    <div class="row">
-      <?php if (!empty($facultyList)): ?>
-        <?php foreach ($facultyList as $faculty): ?>
-        <?php $token = base64_encode($faculty['code'] . '|' . $faculty['code']); ?>
+<section class="section profile">
+  <?php if (!empty($groupedFaculty)): ?>
+    <?php foreach ($groupedFaculty as $department => $facultyMembers): ?>
+      <div class="row mb-4">
+        <div class="col-12">
+          <h4 class="text-primary"><?= htmlspecialchars($department) ?></h4>
+          <hr>
+        </div>
 
+        <?php foreach ($facultyMembers as $faculty): ?>
+          <?php $token = base64_encode($faculty['code'] . '|' . $faculty['code']); ?>
           <div class="col-xl-4">
             <div class="card">
               <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
@@ -39,17 +54,21 @@ $facultyList = $vm->getFacultyInfo();
               </div>
               <div class="card-footer bg-primary-subtle">
                 <a href="submitEvaluation?token=<?= urlencode($token); ?>" title="View">
-                    <div class="btn btn-info mt-1 px-1 btn-sm text-white"><i class="bi bi-journal-text"></i> Submit Evaluation</div>
+                  <div class="btn btn-info mt-1 px-1 btn-sm text-white">
+                    <i class="bi bi-journal-text"></i> Submit Evaluation
+                  </div>
                 </a>
               </div>
             </div>
           </div>
         <?php endforeach; ?>
-      <?php else: ?>
-        <p class="text-danger">No Registered Faculty Found</p>
-      <?php endif; ?>
-    </div>
-  </section>
+      </div>
+    <?php endforeach; ?>
+  <?php else: ?>
+    <p class="text-danger">No Registered Faculty Found</p>
+  <?php endif; ?>
+</section>
+
 </main>
 
 
