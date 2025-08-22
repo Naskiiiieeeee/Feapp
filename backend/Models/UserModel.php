bcb23328b01d9395d8db34ab51d85816f3789011
Naskiiiieeeee <humblebeast1218@gmail.com>
2025-08-22 19:07:27 +0800

    Faculty and administrator login issues fixed

--- backend/Models/UserModel.php
+++ backend/Models/UserModel.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../Core/BaseModel.php';
 
 class UserModel extends BaseModel {
     public function getUserByEmail($email) {
-        $stmt = $this->db->prepare("SELECT * FROM `endusers` WHERE `email` = ?");
+        $stmt = $this->db->prepare("SELECT * FROM `endusers` WHERE `email` = ? AND `status` = 1 ");
         $stmt->execute([$email]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
